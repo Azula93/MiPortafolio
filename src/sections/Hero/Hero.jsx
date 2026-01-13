5a7311d79131aaae31dd61116c64b6b5847d2ca7
Azula Dev <azuladev93@gmail.com>
2026-01-12 23:33:05 -0500

    update subtitle

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -32,7 +32,7 @@ function Hero() {
         <h1>
           Silvia <br /> Riquett
         </h1>
-        <h2>Frontend Developer</h2>
+        <h2>Fullstack Developer | Cybersecurity & Infrastructure</h2>
         <span>
           <a href="https://github.com/Azula93" target="blank">
             <img src={githubIcon} alt="github icon" />
